a9a680ff043f1b412d00ff67fc4b68004e5ca406
Vincent Walker <vincethewalker@yahoo.co.uk>
2018-09-04 11:34:50 +0000

    Greeting function

---
+++ repos/js_tutorial/day.js
@@ -0,0 +1,11 @@
+function dayName(date){
+        const daysOfTheWeek = ["Sunday", "Monday", "Tuesday",
+                                "Wednesday", "Thursday", "Friday", "Saturday"];
+        return daysOfTheWeek[date.getDay()];
+      }
+
+      function greeting() {
+          return  "Hello world! Happy";
+          
+          
+      }
